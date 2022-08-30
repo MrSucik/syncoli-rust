198017a44b3b7047452df9918b0240d04297a692
Daniel Suchan <mr.sucik@gmail.com>
2022-08-30 13:00:24 +0200

    feat: run vlc loop and fullscreen

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     let video_file = &"/home/master/subway.mp4";
     Command::new("cvlc")
         .arg(video_file)
+        .arg("--loop")
+        .arg("--fullscreen")
         .spawn()
         .expect("failed to execute process");
 }
